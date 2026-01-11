d8bddfad6bdd2d7207a97238d884a7e0f58e5ca5
Daria <darabalandina348@gmail.com>
2026-01-11 21:52:47 +0300

    изменение

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.generic import RedirectView
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Главная страница
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
+    # Админка Django
     path('admin/', admin.site.urls),
-    path('books/', include('books.urls')),
-    path('users/', include('users.urls')),
-    path('analytics/', include('analytics.urls')),
-    path('', RedirectView.as_view(url='/books/', permanent=True)),
+
+    # Аутентификация (вход/выход)
+    path('login/', auth_views.LoginView.as_view(
+        template_name='registration/login.html',
+        redirect_authenticated_user=True
+    ), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='/'
+    ), name='logout'),
+
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# Кастомизация заголовков админки
-admin.site.site_header = "Book Mood Matcher - Панель администратора"
-admin.site.site_title = "Админка Book Mood Matcher"
-admin.site.index_title = "Добро пожаловать в систему подбора книг!"
